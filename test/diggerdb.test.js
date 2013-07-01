5232c70223f0d7b22a65a752a2af9e175c5fe760
Kai Davenport <kaiyadavenport@gmail.com>
2013-07-01 15:30:01 +0000

    big data test

--- test/diggerdb.test.js
+++ test/diggerdb.test.js
@@ -402,4 +402,100 @@ describe('diggerdb', function(){
 
 	})
 
+	it('should do lots of data and stay intact', function(testdone){
+		
+		/*
+		
+			30 seconds
+			
+		*/
+		this.timeout(30 * 1000);
+		
+		var data = require(__dirname + '/fixtures/data').citiesxml;
+		
+		var db = diggerdb({
+			database:'bigtest',
+			collection:'bigtest',
+			url:'/',
+			reset:true
+		})
+
+		var dbcontainer = digger.supplychain('/', db);
+		var rootcontainer = digger.create('folder');
+
+		/*
+		
+			add X sets each with Y levels
+			
+		*/
+		function createdata(){
+			return digger.container(data);
+		}
+
+		function insertdata(sets, levels, alldone){
+			
+
+			var currentset = 0;
+
+			async.whilst(
+				function(){
+					currentset++;
+					return currentset<sets;
+				},
+				function(nextset){
+					var currentlevel = 0;
+					var lastcontainer = rootcontainer;
+
+					async.whilst(
+						function(){
+							currentlevel++;
+							return currentlevel<levels;
+						},
+						function(nextlevel){
+							var appendcontainer = createdata();
+
+							lastcontainer
+								.append(appendcontainer)
+
+							lastcontainer = appendcontainer.find('area[name=Meadows]');
+
+							nextlevel();
+
+						},
+						nextset
+					)
+				},
+				function(){
+
+					dbcontainer
+						.append(rootcontainer)
+						.ship(function(){
+							async.parallel({
+								poor:function(done){
+									dbcontainer('city.south area.poor').ship(function(areas){
+										done(null, areas.count());
+									})
+								},
+								rich:function(done){
+									dbcontainer('city.north area.rich').ship(function(areas){
+										done(null, areas.count());
+									})
+								}
+							}, function(error, results){
+								results.poor.should.equal(459);
+								results.rich.should.equal(300);
+								alldone();
+							})
+							
+						})
+					
+					
+				}
+			)
+		}
+		
+		insertdata(10, 10, testdone);
+		
+	})
+
 })
